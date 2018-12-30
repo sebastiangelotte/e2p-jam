211d49782e0b34c462eded70a8ae76c21efa9dcc
Sebastian Gelotte <sebastian.gelotte@gmail.com>
2018-12-30 05:48:16 +0100

    Add manifest.json support

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,6 +29,20 @@ module.exports = {
     {
       resolve: 'gatsby-source-contentful',
       options: contentfulConfig,
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: `standalone`,
+        icon: `static/icon.png`, // This path is relative to the root of the site.
+      },
     }
   ],
 }
